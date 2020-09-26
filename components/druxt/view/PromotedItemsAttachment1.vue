69d3ca443171a0f56cc53319dfc4ab1545bfaa81
Stuart Clark <stuart@realityloop.com>
2020-09-27 09:41:20 +1000

    [#14] Added theming of Promoted Items attachment.

---
+++ components/druxt/view/PromotedItemsAttachment1.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-row class="h-100">
+    <slot
+      name="results"
+      :wrapper="{
+        component: 'b-col',
+        props: {
+          class: 'mb-3 mb-sm-0',
+          cols: 12,
+          sm: 6
+        }
+      }"
+    />
+  </b-row>
+</template>
+
+<script>
+import { DruxtViewsViewMixin } from 'druxt-views'
+
+export default {
+  mixins: [DruxtViewsViewMixin],
+}
+</script>
