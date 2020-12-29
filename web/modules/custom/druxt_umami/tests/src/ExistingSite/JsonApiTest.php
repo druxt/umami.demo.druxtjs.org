9b0fd20d4f0a503415a7c8220af307dcc0e51f67
Stuart Clark <stuart@realityloop.com>
2020-12-29 19:53:52 +1100

    [#13] Added JSON:API tests.

--- web/modules/custom/druxt_umami/tests/src/ExistingSite/JsonApiTest.php
+++ web/modules/custom/druxt_umami/tests/src/ExistingSite/JsonApiTest.php
@@ -9,12 +9,55 @@ class JsonApiTest extends ExistingSiteBase {
     parent::setUp();
   }
 
+  public function testApi() {
+    $this->visit('/jsonapi');
+    $this->assertSession()->statusCodeEquals(200);
+  }
+
   public function testRouter() {
+    // Homepage.
     $this->visit('/router/translate-path?path=/');
     $this->assertSession()->statusCodeEquals(200);
 
     $data = json_decode($this->getCurrentPageContent(), TRUE);
     $this->assertArrayHasKey('resolved', $data);
     $this->assertEquals($data['resolved'], 'http://nginx:8080/en/node');
+
+    // Node 1.
+    $this->visit('/router/translate-path?path=/node/1');
+    $this->assertSession()->statusCodeEquals(200);
+
+    $data = json_decode($this->getCurrentPageContent(), TRUE);
+    $this->assertArrayHasKey('resolved', $data);
+    $this->assertEquals($data['resolved'], 'http://nginx:8080/en/recipes/deep-mediterranean-quiche');
+  }
+
+  public function testJsonApiViews() {
+    // Router.
+    $this->visit('/router/translate-path?path=/');
+    $this->assertSession()->statusCodeEquals(200);
+
+    $route = json_decode($this->getCurrentPageContent(), TRUE);
+    $this->assertArrayHasKey('view', $route);
+    $this->assertArrayHasKey('jsonapi_views', $route);
+    $this->assertEquals($route['view']['view_id'], 'frontpage');
+    $this->assertEquals($route['view']['display_id'], 'page_1');
+
+    // View resource.
+    $this->visit("/jsonapi/view/view/{$route['view']['uuid']}");
+    $this->assertSession()->statusCodeEquals(200);
+
+    $view = json_decode($this->getCurrentPageContent(), TRUE);
+    $this->assertArrayHasKey('data', $view);
+    $this->assertEquals($view['data']['type'], 'view--view');
+    $this->assertEquals($view['data']['id'], $route['view']['uuid']);
+
+    // JSON:API Views resource.
+    $this->visit($route['jsonapi_views']);
+    $this->assertSession()->statusCodeEquals(200);
+
+    $results = json_decode($this->getCurrentPageContent(), TRUE);
+    $this->assertArrayHasKey('data', $results);
+    $this->assertCount(4, $results['data']);
   }
 }
