9c81ddc12df784841517e4fd12dea1f415e2bc57
Brian Gilbert <brian@realityloop.com>
2021-04-27 14:39:27 +1000

    [#183] Fix initial viewport on mobile devices.

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
