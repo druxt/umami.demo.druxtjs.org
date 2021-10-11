726edb134c7a55304df089e37625a886cca6a066
Stuart Clark <stu@rtclark.net>
2021-10-12 08:09:09 +1100

    feat(#321): add featured articles view block (#322)
    * chore(#321): update DruxtSite
    
    * feat(#321): add articles_aside views block

--- components/druxt/entity/node/article/Default.vue
+++ components/druxt/entity/node/article/Default.vue
@@ -27,9 +27,25 @@
         </b-row>
       </b-col>
 
-      <!-- <b-col md="4">
-        <druxt-block-region name="sidebar" :theme="theme" />
-      </b-col> -->
+      <b-col md="4">
+        <DruxtView
+          :arguments="[entity.attributes.drupal_internal__nid]"
+          display-id="block_1"
+          view-id="articles_aside"
+        >
+          <template #default="{ display, results }">
+            <h3 class="mt-4 mb-3" v-text="display.display_options.title" />
+            <DruxtEntity
+              v-for="result of results"
+              :key="result.id"
+              class="mb-3"
+              mode="card"
+              :type="result.type"
+              :uuid="result.id"
+            />
+          </template>
+        </DruxtView>
+      </b-col>
     </b-row>
   </article>
 </template>
@@ -42,6 +58,12 @@ export default {
 
   computed: {
     theme: () => 'umami',
+  },
+
+  druxt: {
+    query: {
+      fields: ['drupal_internal__nid'],
+    },
   },
 }
 </script>
