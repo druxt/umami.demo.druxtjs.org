c5d52c822f43b4246638db4a44859ac69c71f3cd
Stuart Clark <stu@rtclark.net>
2022-01-14 08:45:43 +1100

    [#62] Update ddev to 1.18.2 (#63)
    * chore(#62): update gitpod setup
    
    * chore(#62): update ddev config
    
    * chore(#62): add ddev drupal-install command

--- .gitpod/scripts/drupal-setup.sh
+++ .gitpod/scripts/drupal-setup.sh
@@ -3,9 +3,4 @@
 set -eu -o pipefail
 
 # Set up Drupal website
-ddev composer install
-ddev drush -y si demo_umami
-ddev drush -y en druxt_umami
-ddev drush rap anonymous 'access\ druxt\ resources'
-ddev drush config:set jsonapi.settings read_only 0 -y
-ddev drush search-api:index
+ddev drupal-install
