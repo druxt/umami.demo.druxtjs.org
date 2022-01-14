169f4abdacbfdc5cae1f6e18f16c5af485b0895e
Stuart Clark <stu@rtclark.net>
2022-01-14 13:19:31 +1100

    fix(#365): update promoted items block (#366)

--- components/druxt/block-region/BannerTop.vue
+++ components/druxt/block-region/BannerTop.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <slot v-if="route.isHomePath" name="umami_banner_home" />
-    <slot v-if="route.isHomePath" name="views_block__promoted_items_block_1" />
+    <slot v-if="route.isHomePath" name="umami_views_block__promoted_items_block_1" />
     <slot
       v-if="route.resolvedPath === '/en/recipes'"
       name="umami_banner_recipes"
