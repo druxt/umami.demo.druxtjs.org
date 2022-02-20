37828a4ce93925fe4f485bbfb4694c653cf197da
Brian Gilbert <brian@realityloop.com>
2022-02-21 09:29:17 +1100

    feat(#76): enable xdebug in gitpod vscode (#77)

--- .gitpod/scripts/ddev-setup.sh
+++ .gitpod/scripts/ddev-setup.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+# Download ddev's images
+ddev debug download-images
+
+# Set up ddev for use on gitpod
+DRUPAL_DIR="${GITPOD_REPO_ROOT}"
+
 # Misc housekeeping before start
 ddev config global --instrumentation-opt-in=true
 
 # Start ddev
-ddev start
+cd $DRUPAL_DIR && ddev start -y
