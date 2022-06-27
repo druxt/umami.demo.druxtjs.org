a915af5b5f27407e0652ef1378dd3f1a01335c1c
Stuart Clark <stu@rtclark.net>
2022-06-27 22:59:44 +0000

    fix(#434): add vuex paths and exclude route

--- plugins/vuex-persistedstate.client.js
+++ plugins/vuex-persistedstate.client.js
@@ -1,5 +1,15 @@
 import createPersistedState from 'vuex-persistedstate'
 
 export default ({store}) => {
-  createPersistedState({ key: 'druxtCache' })(store)
+  createPersistedState({
+    key: 'druxtCache',
+    paths: [
+      'druxtRouter.routes',
+      'druxt/views.results',
+      'druxtMenu.entities',
+      'druxtSchema.schemas',
+      'druxt.collections',
+      'druxt.resources'
+    ]
+  })(store)
 }
